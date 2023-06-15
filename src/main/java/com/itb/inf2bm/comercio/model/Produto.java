b332b42838830940aea80c504c6edd3751683d58
Gabriel <bielss1609@gmail.com>
2023-06-15 09:39:52 -0300

    Projeto

---
+++ src/main/java/com/itb/inf2bm/comercio/model/Produto.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.itb.inf2bm.comercio.model;
+
+/**
+ * @author rm86059
+ *
+ */
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String codigoBarras;
+	private double preco;
+	
+	//Private: Acesso liberado aprenas para os membros da própria classe.
+	
+	
+}
