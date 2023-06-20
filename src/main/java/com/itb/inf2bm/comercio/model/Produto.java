dc115a79902be4b1e54a7b3e26b9a2dc8a93f53a
Gabriel Santos de Souza <rm86059@estudante.fieb.edu.br>
2023-06-20 09:05:08 -0300

    Getter and Setters

--- src/main/java/com/itb/inf2bm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2bm/comercio/model/Produto.java
@@ -17,5 +17,39 @@ public class Produto {
 	
 	//Private: Acesso liberado aprenas para os membros da própria classe.
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
 	
 }
